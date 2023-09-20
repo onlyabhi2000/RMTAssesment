df99c12322de83323386d46065d1c1c223c316ea
abhisharma2000 <abhishek@ramailo.tech>
2023-09-20 17:53:11 +0530

    day1 assessment

---
+++ src/main/java/com/abhishek/rmtassessment/RmtAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.rmtassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmtAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmtAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhishek/rmtassessment/RmtAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.rmtassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmtAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
